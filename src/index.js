c3cdf8002fce7aaf707614ba3f17cba924b332d2
j23saw <jeshsawant23@gmail.com>
2020-04-20 15:33:08 +0530

    first-commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Workout</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
